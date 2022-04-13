3319e7694af2064a3cc97d7eb07268f50d3312e9
Barnaby Keene <accounts@southcla.ws>
2022-04-13 16:43:57 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+	"regexp"
+)
+
+var re = regexp.MustCompile(`(?m)\$env:(.+) = (.+)`)
+
+func main() {
+	in := bufio.NewScanner(os.Stdin)
+	out := json.NewEncoder(os.Stdout)
+
+	table := map[string]string{}
+
+	for in.Scan() {
+		line := in.Text()
+
+		matches := re.FindAllStringSubmatch(line, -1)[0]
+		key := matches[1]
+		value := matches[2]
+
+		table[key] = value
+	}
+
+	out.Encode(table)
+}
